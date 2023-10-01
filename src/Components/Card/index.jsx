d6da6faafe2da90b39762873bf8d93b38a934567
Roxanne Vignon <vignon.roxanne@gmail.com>
2023-10-01 22:09:18 +0200

    scss cartes

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -2,10 +2,10 @@ import './card.scss';
 
 const Card = () => {
     return (
-        <div className="card">
-            <img src='#' alt="location" className="card__img" />
+        <article className="card">
+            <img src='https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg' alt="location" className="card__img" />
             <p className="card__title">Titre de la location</p>
-        </div>
+        </article>
     )
 }
 
